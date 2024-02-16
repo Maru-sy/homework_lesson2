3fe21026988d47bc77281b9b7b883118994b4f7a
Mariya <marusyaershova532@gmail.com>
2024-02-16 16:25:57 +0200

    created homework3 file, added task1 to homework3

---
+++ homework3.py
@@ -0,0 +1,26 @@
+try:
+    user_select = int(input("Enter day number: "))
+    match user_select:
+        case 1:
+            print("Monday")
+        case 2:
+            print("Tuesday")
+        case 3:
+            print("Wednesday")
+        case 4:
+            print("Thursday")
+        case 5:
+            print("Friday")
+        case 6:
+            print("Saturday")
+        case 7:
+            print("Sunday")
+        case _:
+            print("Incorrect day number")
+except ValueError as error:
+    print(f"Enter only integer numbers,please.")
+    print(f"ValueError: {error}")
+except Exception as error:
+    print(f"Exception occurred: {error}")
+finally:
+    print("End")
